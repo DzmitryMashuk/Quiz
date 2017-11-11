51bd94b8e54c38e54aa956ee4c9894ae347b14c6
Mashuk Dzmitry <DzmitryMashuk@gmail.com>
2017-11-11 22:50:16 +0300

    work next quiz

--- src/AppBundle/Controller/Quiz/QuizPageController.php
+++ src/AppBundle/Controller/Quiz/QuizPageController.php
@@ -19,7 +19,6 @@ class QuizPageController extends Controller
      */
     public function userQuizPageAction(Request $request)
     {
-        $userId = $request->get('idUser');
         $quizName = $request->get('quizName');
         $em = $this->getDoctrine()->getManager();
 
@@ -29,11 +28,12 @@ class QuizPageController extends Controller
         $answer = $em->getRepository(Answer::class)->find($quizQuestion[0]->getIdAnswer());
 
         return $this->render('quiz/userQuizPage.html.twig', array(
-            'idUser' => $userId,
+            'idUser' => $request->get('idUser'),
             'quizName' => $quizName,
             'idQuizQuestion' => $quizQuestion[0]->getId(),
             'idQuestion' => $question->getId(),
             'idAnswer' => $answer->getId(),
+            'idUserAnswer' => null,
             'whatQuestion' => 0,
             'question' => $question->getQuestion(),
             'answer1' => $answer->getAnswer1(),
@@ -68,6 +68,7 @@ class QuizPageController extends Controller
         'idQuizQuestion' => $idQuizQuestion,
         'idQuestion' => $request->get('idQuestion'),
         'idAnswer' => $request->get('idAnswer'),
+        'idUserAnswer' => $userAnswer->getId(),
         'whatQuestion' => $whatQuestion,
         'question' => $request->get('question'),
         'answer1' => $request->get('answer1'),
@@ -82,10 +83,29 @@ class QuizPageController extends Controller
      */
     public function nextQuizPageAction(Request $request)
     {
-        
+        $whatQuestion = $request->get('whatQuestion') + 1;
+        $quizName = $request->get('quizName');
+        $idUserAnswer = $request->get('idUserAnswer');
+        $em = $this->getDoctrine()->getManager();
+
+        $quiz = $em->getRepository(Quiz::class)->findOneBy(['name' => $quizName]);
+        $quizQuestion = $em->getRepository(QuizQuestion::class)->findBy(['idQuiz' => $quiz->getId()]);
+        $question = $em->getRepository(Question::class)->find($quizQuestion[$whatQuestion]->getIdQuestion());
+        $answer = $em->getRepository(Answer::class)->find($quizQuestion[$whatQuestion]->getIdAnswer());
 
         return $this->render('quiz/userQuizPage.html.twig', array(
-        'currentAnswer' => $request->get('currentAnswer')
+            'idUser' => $request->get('idUser'),
+            'quizName' => $quizName,
+            'idQuizQuestion' => $quizQuestion[$whatQuestion]->getId(),
+            'idQuestion' => $question->getId(),
+            'idAnswer' => $answer->getId(),
+            'idUserAnswer' => $request->get('idUserAnswer'),
+            'whatQuestion' => $whatQuestion,
+            'question' => $question->getQuestion(),
+            'answer1' => $answer->getAnswer1(),
+            'answer2' => $answer->getAnswer2(),
+            'answer3' => $answer->getAnswer3(),
+            'answer4' => $answer->getAnswer4()
         ));
     }
 
