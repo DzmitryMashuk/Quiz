b123e162b9f9e92fd948f6b13ac3c2945ad7e6c2
Mashuk Dzmitry <DzmitryMashuk@gmail.com>
2017-11-13 09:02:48 +0300

    add Declare

--- src/AppBundle/Form/LoginType.php
+++ src/AppBundle/Form/LoginType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AppBundle\Form;
 
 
@@ -7,9 +9,7 @@ use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class LoginType extends AbstractType
